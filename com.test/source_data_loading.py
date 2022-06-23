97c7e9c1a5465d1acf82ff37abee7e674d8cbc07
Amey <ameydhuri55@gmail.com>
2022-06-23 14:28:27 +0530

    updated stg_path in source_data_loading.py

--- com.test/source_data_loading.py
+++ com.test/source_data_loading.py
@@ -34,29 +34,29 @@ if __name__ == '__main__':
         stg_path = 's3a://' + app_conf['s3_conf']['s3_bucket'] + '/' + app_conf['s3_conf']['staging_location'] + '/' + src
 
         # MYSQL Source
-        if src == 'TD':
-            # use the ** operator/un-packer to treat a python dictionary as **kwargs
-            print("\nReading data from MySQL DB using SparkSession.read.format(),")
-            mysql_TD_df = ut.mysql_TD(spark, app_secret, src_config)
-            mysql_TD_df.show()
-            mysql_TD_df.write.partitionBy('insert_date').mode('overwrite').parquet(stg_path)
+        #if src == 'TD':
+        #    # use the ** operator/un-packer to treat a python dictionary as **kwargs
+        #    print("\nReading data from MySQL DB using SparkSession.read.format(),")
+        #    mysql_TD_df = ut.mysql_TD(spark, app_secret, src_config)
+        #    mysql_TD_df.show()
+        #    mysql_TD_df.write.partitionBy('insert_date').mode('overwrite').parquet(stg_path)
 
         # SFTP Source
-        elif src == 'OL':
-            print("\nReading data from SFTP using SparkSession.read.format(),")
-            sftp_OL_df = ut.sftp_OL(spark, current_dir, app_secret, src_config)
-            sftp_OL_df.show(5, False)
-            sftp_OL_df.write.partitionBy('insert_date').mode('overwrite').parquet(stg_path)
+        #elif src == 'OL':
+        #    print("\nReading data from SFTP using SparkSession.read.format(),")
+        #    sftp_OL_df = ut.sftp_OL(spark, current_dir, app_secret, src_config)
+        #    sftp_OL_df.show(5, False)
+        #    sftp_OL_df.write.partitionBy('insert_date').mode('overwrite').parquet(stg_path)
 
         # MONGODB Source
-        elif src == 'CD':
-            print("\nReading data from MONGODB using SparkSession.read.format(),")
-            mongodb_CD_df = ut.mongodb_CD(spark, src_config["mongodb_config"]["database"], src_config["mongodb_config"]["collection"])
-            mongodb_CD_df.show()
-            mongodb_CD_df.write.partitionBy('insert_date').mode('overwrite').parquet(stg_path)
+        #elif src == 'CD':
+        #    print("\nReading data from MONGODB using SparkSession.read.format(),")
+        #    mongodb_CD_df = ut.mongodb_CD(spark, src_config["mongodb_config"]["database"], src_config["mongodb_config"]["collection"])
+        #    mongodb_CD_df.show()
+        #    mongodb_CD_df.write.partitionBy('insert_date').mode('overwrite').parquet(stg_path)
 
         # S3 Source
-        elif src == 'CP':
+        if src == 'CP':
             print("\nReading data from S3 Bucket using SparkSession.read.format(),")
             s3_bucket_CP_df = ut.s3_bucket_CP(spark)
             s3_bucket_CP_df.show(5, False)
