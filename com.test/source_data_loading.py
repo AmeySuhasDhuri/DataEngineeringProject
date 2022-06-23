dea78634f62495f242835c5d6bc92bfa3cda6ccc
Amey <ameydhuri55@gmail.com>
2022-06-23 14:25:26 +0530

    updated stg_path in source_data_loading.py

--- com.test/source_data_loading.py
+++ com.test/source_data_loading.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
         .builder \
         .appName("Read com.test enterprise applications") \
         .master('local[*]') \
+        .config('spark.jars.packages', 'com.springml:spark-sftp_2.11:1.1.1') \
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
 
